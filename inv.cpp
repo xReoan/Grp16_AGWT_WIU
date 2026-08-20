1bf31b35a6017b12059a52fcccc13db8aa0d9271
icansoletmein <xuan8906@gmail.com>
2026-08-20 10:43:17 +0800

    Add files via upload

---
+++ inv.cpp
@@ -0,0 +1,52 @@
+#include "inv.h"
+#include "item.h"
+#include "itemDatabase.h"
+#include <iostream>
+inv::inv() {
+    for (int i = 0; i < 10;i++) {
+        invitem[i] = database.getitem(i);
+    }
+}
+
+//saves the number of the number base on the item database, -1 is none
+int inv::invinside[16] = {0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+void inv::OpenInv() const
+{
+    for (int i = 0; i < 16; i++) {
+        if (inv::invinside[i] != -1 ) {
+            std::cout << i + 1<<". " << invitem[inv::invinside[i]]->getname() << std::endl;
+        }
+        else {
+            std::cout << i + 1 << ". Empty" << std::endl;
+        }
+    }
+}
+
+//takes a int cuz thats how its storied, will be changed
+void inv::RecivedInv(int thing) const
+{
+    char changed = 'N';
+    for (int i = 0; i < 16; i++) {
+        if (invinside[i] == -1) {
+            invinside[i] = thing;
+            changed = 'Y';
+            return;
+        }
+    }
+    if (changed != 'Y') {
+        std::cout << "Invantory full, cant store" << std::endl;
+    }
+}
+
+//like which of the 16 items slot is the item they used is from, can be changed
+void inv::UsedInv(int itemSlot) const
+{
+    if (invinside[itemSlot] != -1) {
+        std::cout << "Used " << invitem[invinside[itemSlot]]->getname() << std::endl;;
+        invinside[itemSlot] = -1;
+    }
+    else {//if it is emepty
+        std::cout << "no item to use"<<std::endl;
+    }
+}
