7184010655188b23c957684bb39787c3b486c74d
Venise Lim <251218b@mymail.nyp.edu.sg>
2026-08-24 10:16:20 +0800

    added comments

--- revisedshop.cpp
+++ revisedshop.cpp
@@ -11,8 +11,8 @@ int revisedshop::weaponItems[5] = { 1, 2, 3, 4, 5 };
 
 revisedshop::revisedshop() : coins()
 {
-    for (int i = 0; i < 10; i++) { // process each index in the shopItems + shopItemPtrs arrays
-        if (shopItems[i] != -1) { // check value at i in shopItems is NOT -1 = " slot is empty "
+    for (int i = 0; i < 10; i++) { // for each index in the shopItems + shopItemPtrs arrays
+        if (shopItems[i] != -1) { // check value at i in shopItems is NOT -1 = " slot is empty ", have "-1" there because all valid items start from 0, therefore "-1" cannot refer to any valid items, considering it a "item invalid/empty"
             shopItemPtrs[i] = database.getitem(shopItems[i]); 
         }
         else {
@@ -56,7 +56,7 @@ void revisedshop::ShopMenu()
             std::cout << "\nEnter slot number to buy (1-10) or 0 to cancel: ";
             std::cin >> slotChoice;
             if (slotChoice > 0 && slotChoice <= 10) {
-                BuyItem(slotChoice - 1);
+				BuyItem(slotChoice - 1); // do a "- 1" so that the user can input 1-10, but the array is 0-9
             }
             std::cin.ignore();
             std::cin.get();
@@ -97,24 +97,28 @@ void revisedshop::ShopMenu()
     }
 }
 
-void revisedshop::OpenShop() const
+// shop items displayed after "I"
+void revisedshop::OpenShop() const // const so it doesnt change any data
 {
     std::cout << "----------------------------------------------------" << std::endl;
     std::cout << "                    ITEMS FOR SALE                  " << std::endl;
     std::cout << "----------------------------------------------------" << std::endl;
 
     for (int i = 0; i < 9; i++) {
-        if (shopItems[i] != -1 && shopItemPtrs[i] != nullptr) {
-            std::cout << " [" << i + 1 << "] " << shopItemPtrs[i]->getname()
-                << " - " << shopItemPtrs[i]->getprice() << " coins" << std::endl;
+        if (shopItems[i] != -1 && shopItemPtrs[i] != nullptr) { // if the item is NOT -1 and NOT nullptr=meaaning it exists in memory
+            std::cout << " [" << i + 1 << "] " << shopItemPtrs[i]->getname() // use nullptr as a checker/a sential value as it'll mean nothing is there, this also prevents crashes
+                << " - " << shopItemPtrs[i]->getprice() << " coins" << std::endl; // nullptr instead of 0 because 0 can be mistaken as an integer but nullptr refers to only a pointer.
 			std::cout << "     " << shopItemPtrs[i]->getdescription() << std::endl;
             if (shopItemPtrs[i]->getdefensevalue() != 0) {
+				// if the item has defense value
                 std::cout << "    Defense Value: " << shopItemPtrs[i]->getdefensevalue() << std::endl;
             }
             if (shopItemPtrs[i]->gethealvalue() != 0) {
+                // if item has health value
                 std::cout << "    Health Value: " << shopItemPtrs[i]->gethealvalue() << std::endl;
             }
 			if (shopItemPtrs[i]->getduration() != 0) {
+                // if item has a duration
 					std::cout << "    Duration: " << shopItemPtrs[i]->getduration() << " turns" << std::endl;
 				
 			}
@@ -126,6 +130,7 @@ void revisedshop::OpenShop() const
     std::cout << "----------------------------------------------------" << std::endl;
 }
 
+// weapon items displayed after "W"
 void revisedshop::OpenWeaponShop() const
 {
     std::cout << "----------------------------------------------------" << std::endl;
@@ -137,6 +142,7 @@ void revisedshop::OpenWeaponShop() const
             std::cout << " [" << i + 1 << "] " << weaponItemPtrs[i]->getname()
                 << " - " << weaponItemPtrs[i]->getprice() << " coins" << std::endl;
             std::cout << "    " << weaponItemPtrs[i]->getdescription() << std::endl;
+            // if weapon has attack value
             if (weaponItemPtrs[i]->getattackvalue() != 0) {
                 std::cout << "    Attack Value: " << weaponItemPtrs[i]->getattackvalue() << std::endl;
             }
@@ -148,18 +154,19 @@ void revisedshop::OpenWeaponShop() const
     std::cout << "----------------------------------------------------" << std::endl;
 }
 
-void revisedshop::BuyItem(int itemSlot) const
+void revisedshop::BuyItem(int itemSlot)
 {
-    if (itemSlot < 0 || itemSlot >= 10) {
+    if (itemSlot < 0 || itemSlot >= 10) { // not in list
         std::cout << "Invalid slot number!" << std::endl;
         return;
     }
 
-    if (shopItems[itemSlot] == -1) {
+	if (shopItems[itemSlot] == -1) { // if the item is -1, it doesn't exist 
         std::cout << "No item in that slot!" << std::endl;
         return;
     }
 
+	// if the player has enough coins to buy the item
     item* selectedItem = shopItemPtrs[itemSlot];
     int price = selectedItem->getprice();
 
@@ -169,10 +176,11 @@ void revisedshop::BuyItem(int itemSlot) const
     }
 
     std::cout << "You bought " << selectedItem->getname() << " for " << price << " coins!" << std::endl;
+    // put the item slot in the inventory
     inv2.RecivedInv(shopItems[itemSlot]);
 }
 
-void revisedshop::BuyWeapon(int weaponSlot) const
+void revisedshop::BuyWeapon(int weaponSlot)
 {
     if (weaponSlot < 0 || weaponSlot >= 10) {
         std::cout << "Invalid weapon slot number!" << std::endl;
@@ -196,27 +204,31 @@ void revisedshop::BuyWeapon(int weaponSlot) const
     inv2.RecivedInv(weaponItems[weaponSlot]);
 }
 
+// CURRENCY ---------------
 void revisedshop::AddCoins(int amount)
 {
     if (amount > 0) {
-        coins += amount;
+        coins += amount; // add amount added to coins
         std::cout << "Added " << amount << " coins. Total: " << coins << std::endl;
     }
 }
 
-int revisedshop::GetCoins() const
+// returns current coin total of the shop
+int revisedshop::GetCoins() const // const=ensure func no change the shop's data aka does nnt modify any variables
 {
-    return coins;
+    return coins; // sends back the coin value if called by diff func
 }
 
+// check if item available
 bool revisedshop::IsItemAvailable(int itemSlot) const
 {
     if (itemSlot >= 0 && itemSlot < 10) {
-        return shopItems[itemSlot] != -1;
+        return shopItems[itemSlot] != -1; // return true/false based on slot
     }
     return false;
 }
 
+//check if weapon available
 bool revisedshop::IsWeaponAvailable(int weaponSlot) const
 {
     if (weaponSlot >= 0 && weaponSlot < 10) {
