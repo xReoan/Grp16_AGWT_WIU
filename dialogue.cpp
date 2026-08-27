551a36189b37064ec31faa178a16b15a47105128
icansoletmein <xuan8906@gmail.com>
2026-08-27 12:28:36 +0800

    fixed di

--- dialogue.cpp
+++ dialogue.cpp
@@ -135,6 +135,7 @@ system("cls");
 }
 
 void dialogue::introafterbattlediag() {
+	system("cls");
 	std::cout << R"(Press Enter to continue . . .
 -------------------------------------------------------------
 15: "Okay then... I'll start by introducing ourselves. 
@@ -189,6 +190,7 @@ subject that arrived here, we refer to him as...")" << std::endl;
 }
 
 void dialogue::battle2diag() {
+	std::cin.get();
 	system("cls");
 	std::cout << R"(Press Enter to continue . . .
 -------------------------------------------------------------
@@ -225,6 +227,7 @@ You: "Ah..."
 }
 
 void dialogue::battle3diag() {
+	std::cin.get();
 	system("cls");
 	std::cout << R"(Press Enter to continue . . .
 -------------------------------------------------------------
@@ -296,6 +299,7 @@ He simply wanted justice."
 }
 
 void dialogue::battle4diag() {
+	std::cin.get();
 	system("cls");
 	std::cout << R"(Press Enter to continue . . .
 -------------------------------------------------------------
@@ -355,6 +359,7 @@ You: "Where is it then?")" << std::endl;
 }
 
 void dialogue::battle5diag() {
+	std::cin.get();
 	system("cls");
 	std::cout << R"(Press Enter to continue . . .
               _..----.._    _
@@ -422,6 +427,7 @@ You: "Oh.")" << std::endl;
 }
 
 void dialogue::bossdiag() {
+	std::cin.get();
 	system("cls");
 	std::cout << R"(Press Enter to continue . . .
 -------------------------------------------------------------
